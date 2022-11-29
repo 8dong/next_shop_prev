1d428a2cda87c9f8852608de5063d102f67e7d79
DongHyun <hyde981114@gmail.com>
2022-11-30 04:11:45 +0900

    feat: index 페이지 내 getServerSideprops 함수 추가
    index 페이지의 경우 pre-rendering 방식중 SSR 방식 사용

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  images: {
+    domains: ['cdn.class101.net']
+  }
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,16 @@
-import type { NextPage } from 'next';
-import Advertisement from '../components/pages/productItems/Advertiesment';
+import type { NextPage, GetServerSideProps } from 'next';
 
-const Home: NextPage = () => {
+import Advertisement from '../components/pages/productItems/Advertiesment';
+import ProductItemList from '../components/pages/productItems/ProductItemList';
+
+import type { Product } from '../data/productsItem';
+
+interface HomeProps {
+  fetchedProductItems: Product[];
+  isDone: boolean;
+}
+
+const Home: NextPage<HomeProps> = ({ fetchedProductItems, isDone }) => {
   return (
     <div>
       <Advertisement
@@ -10,8 +19,25 @@ const Home: NextPage = () => {
         imgSrc='/assets/ad_img.jpeg'
         imgAlt='광고 이미지'
       />
+      <ProductItemList products={fetchedProductItems} isDone={isDone} />
     </div>
   );
+};
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const res = await fetch('http://localhost:3001/api/fetchProductItems', {
+    method: 'POST',
+    body: JSON.stringify({ startIndex: 0 }),
+    headers: {
+      'Content-type': 'application/json'
+    }
+  });
+
+  const { fetchedProductItems, isDone } = await res.json();
+
+  return {
+    props: { fetchedProductItems, isDone }
+  };
 };
 
 export default Home;
