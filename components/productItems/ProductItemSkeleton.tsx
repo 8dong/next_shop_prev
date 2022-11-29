68cff0914f8381343d55630faca596261ada58e2
DongHyun <hyde981114@gmail.com>
2022-11-30 04:26:59 +0900

    feat: ProductItemSkeleton 컴포넌트 추가
    ProductItem의 Skeleton UI 컴포넌트 추가

---
+++ components/productItems/ProductItemSkeleton.tsx
@@ -0,0 +1,103 @@
+import styled from 'styled-components';
+
+const ProductItemSkeleton = () => {
+  return (
+    <ProductItemSkeletonWrapper>
+      <div>
+        <div className='image'></div>
+        <span className='title'></span>
+      </div>
+      <div className='productPrice'>
+        <span className='totalPrice'></span>
+        <span>
+          <span className='installmentPrice'></span>
+          <span className='installment'></span>
+        </span>
+      </div>
+    </ProductItemSkeletonWrapper>
+  );
+};
+
+const ProductItemSkeletonWrapper = styled.li`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  height: 330px;
+  margin-bottom: 30px;
+
+  position: relative;
+
+  .image {
+    width: 100%;
+    height: 200px;
+    margin-bottom: 10px;
+    animation: skeleton-gradient 1.8s infinite ease-in-out;
+  }
+
+  .title {
+    display: block;
+    width: 80%;
+    height: 2em;
+    margin: 0 5px;
+    animation: skeleton-gradient 1.8s infinite ease-in-out;
+  }
+
+  .totalPrice {
+    display: block;
+    width: 30%;
+    height: 1.5em;
+    margin-bottom: 5px;
+    animation: skeleton-gradient 1.8s infinite ease-in-out;
+  }
+
+  .productPrice span:last-child {
+    display: flex;
+  }
+
+  .installmentPrice {
+    display: block;
+    width: 40%;
+    height: 1.5em;
+    margin-right: 5px;
+    animation: skeleton-gradient 1.8s infinite ease-in-out;
+  }
+
+  .installment {
+    display: block;
+    width: 20%;
+    height: 1.5em;
+    animation: skeleton-gradient 1.8s infinite ease-in-out;
+  }
+
+  @keyframes skeleton-gradient {
+    0% {
+      background-color: rgba(165, 165, 165, 0.1);
+    }
+
+    50% {
+      background-color: rgba(165, 165, 165, 0.3);
+    }
+
+    100% {
+      background-color: rgba(165, 165, 165, 0.1);
+    }
+  }
+
+  @keyframes skeleton-gradient {
+    0% {
+      background-color: rgba(165, 165, 165, 0.1);
+    }
+
+    50% {
+      background-color: rgba(165, 165, 165, 0.3);
+    }
+
+    100% {
+      background-color: rgba(165, 165, 165, 0.1);
+    }
+  }
+`;
+
+ProductItemSkeleton.displayName = 'ProductItemSkeleton';
+
+export default ProductItemSkeleton;
