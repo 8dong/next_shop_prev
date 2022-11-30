2a3f0dc73e1eded383c48d7c91e056f810cd6423
DongHyun <hyde981114@gmail.com>
2022-11-30 15:58:43 +0900

    feat: '/cart' 페이지 컴포넌트 추가

---
+++ pages/cart/index.tsx
@@ -0,0 +1,13 @@
+import CartHeader from '../../components/pages/cart/CartHeader';
+import CartProductItemList from '../../components/pages/cart/CartProductItemList';
+
+const Cart = () => {
+  return (
+    <>
+      <CartHeader />
+      <CartProductItemList />
+    </>
+  );
+};
+
+export default Cart;
