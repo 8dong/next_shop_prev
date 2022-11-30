f9e1713f20f769f9426ca770e3d3122fe35119bc
DongHyun <hyde981114@gmail.com>
2022-11-30 17:45:43 +0900

    feat: 장바구니 product 선택 & 헤제 & 제거 기능 추가
    - cartList에 존재하는 product 중 선택된 product들을 selectedProducts 상태로 관리
    
    - allSelectHandler 함수 실행시 cartList 내 모든 product 선택
    
    - removeCartHandler 함수 인수로 'selected' 문자열 전달시
    선택된 모든 product들 cartList에서 제거
    removeCartHandler 함수 인수로 'product' 문자열, product 객체 전달시
    전달된 product는 cartList에서 제거
    
    - isSelectedValid 함수 인수로 product의 id 프로퍼티 전달시
    selectedProducts 내 존재 유무를 boolean으로 반환
    
    - selectHandler 함수 인수로 product 전달시
    selectedProducts 내 전달받은 product 존재시 selectedProducts에서 제거
    존재하지 않는 경우 selectedProducts 내 추가

--- pages/cart/index.tsx
+++ pages/cart/index.tsx
@@ -1,7 +1,67 @@
+import { useState, useContext } from 'react';
+
 import CartHeader from '../../components/pages/cart/CartHeader';
 import CartProductItemList from '../../components/pages/cart/CartProductItemList';
+import CartStateContext from '../../store/CartStateContext';
+import CartDispatchContext from '../../store/CartDispatchContext';
+
+import type { Product } from '../../data/productsItem';
 
 const Cart = () => {
+  const cartList = useContext(CartStateContext)!;
+  const [selectedProducts, setSelectedProducts] = useState(cartList);
+
+  const { editCartList } = useContext(CartDispatchContext)!;
+
+  const allSelectHandler = () => {
+    setSelectedProducts([...cartList]);
+  };
+
+  const removeCartHandler = (target: 'selected' | 'product', removedProduct?: Product) => {
+    return () => {
+      let newCartList = [];
+      let newSelectedProducts = [];
+
+      if (target === 'selected') {
+        newCartList = cartList.filter((cartProduct) => {
+          return !selectedProducts.some((selectedProduct) => selectedProduct.id === cartProduct.id);
+        });
+      } else if (target === 'product' && removedProduct) {
+        newCartList = cartList.filter((cartproduct) => {
+          return cartproduct.id !== removedProduct.id;
+        });
+
+        newSelectedProducts = selectedProducts.filter(
+          (selectedProduct) => selectedProduct.id !== removedProduct.id
+        );
+
+        editCartList(newCartList);
+        setSelectedProducts(newSelectedProducts);
+      }
+    };
+  };
+
+  const isSelectedValid = (productId: string) => {
+    return selectedProducts.some((selectedProduct) => selectedProduct.id === productId);
+  };
+
+  const selectHandler = (product: Product) => {
+    return () => {
+      const isSelected = isSelectedValid(product.id);
+      let newSelectedProducts;
+
+      if (isSelected) {
+        newSelectedProducts = selectedProducts.filter(
+          (selectedProduct) => selectedProduct.id !== product.id
+        );
+      } else {
+        newSelectedProducts = [...selectedProducts, product];
+      }
+
+      setSelectedProducts(newSelectedProducts);
+    };
+  };
+
   return (
     <>
       <CartHeader />
