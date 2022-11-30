2bf4128a68d81369cbb696e9b4c5cc6566af003a
DongHyun <hyde981114@gmail.com>
2022-11-30 18:55:53 +0900

    feat: CouponList 컴포넌트 추가

---
+++ components/pages/cart/CouponList.tsx
@@ -0,0 +1,91 @@
+import { useState } from 'react';
+import styled from 'styled-components';
+
+import Button from '../../common/Button';
+
+import type { Coupon } from '../../../data/coupons';
+
+interface CouponListProps {
+  coupons: Coupon[];
+  couponSelectHandler: Function;
+  selectedCoupon: Coupon | null;
+}
+
+const CouponList = ({ coupons, couponSelectHandler, selectedCoupon }: CouponListProps) => {
+  const [isShowCouponList, setIsShowCouponList] = useState(false);
+
+  const couponListButtonClickHandler = () => {
+    setIsShowCouponList((prevState) => !prevState);
+  };
+
+  const couponClickHandler = (coupon: Coupon) => {
+    return () => {
+      couponSelectHandler(coupon);
+      setIsShowCouponList(false);
+    };
+  };
+
+  return (
+    <CouponListWrapper isShow={isShowCouponList}>
+      <strong>쿠폰</strong>
+      <div className='couponSelectBox'>
+        <button onClick={couponListButtonClickHandler}>
+          {selectedCoupon ? selectedCoupon.title : '선택해주세요'}
+        </button>
+        {isShowCouponList && (
+          <ul>
+            {coupons.map((coupon) => (
+              <li onClick={couponClickHandler(coupon)} key={coupon.title}>
+                {coupon.title}
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
+    </CouponListWrapper>
+  );
+};
+
+const CouponListWrapper = styled.div<{ isShow: boolean }>`
+  padding: 20px;
+
+  strong {
+    display: block;
+    margin-bottom: 20px;
+
+    font-size: 20px;
+  }
+
+  .couponSelectBox {
+    position: relative;
+  }
+
+  .couponSelectBox button {
+    width: 300px;
+    height: 50px;
+    padding-left: 15px;
+    border: 1px solid #d4d4d4;
+
+    text-align: left;
+  }
+
+  .couponSelectBox ul {
+    position: absolute;
+    top: 100%;
+  }
+
+  .couponSelectBox li {
+    display: flex;
+    align-items: center;
+    width: 300px;
+    height: 50px;
+    padding-left: 15px;
+    border: 1px solid #d4d4d4;
+    border-top: none;
+    background-color: #fff;
+
+    text-align: left;
+  }
+`;
+
+export default CouponList;
